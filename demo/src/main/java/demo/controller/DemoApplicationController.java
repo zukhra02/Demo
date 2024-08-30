ae4e9d94556f581f1bf13756bd0a0356cf3ba05e
zukhra02 <zbattalova@yahoo,es>
2024-08-30 04:22:02 +0200

    Se han hecho los cambios segun las indicaciones

--- demo/src/main/java/demo/controller/DemoApplicationController.java
+++ demo/src/main/java/demo/controller/DemoApplicationController.java
@@ -1,4 +1,4 @@
-package HelloWorldApplication.example.demo;
+package demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplicationController {
 
-
-    @RequestMapping
+    // Mapping for the root endpoint
+    @RequestMapping("/")
     public String helloWorld() {
         return "Hello World from Spring Boot";
     }
+
+    // Mapping for the /goodbye endpoint
     @RequestMapping("/goodbye")
     public String goodbye() {
         return "Goodbye from Spring Boot";
     }
 }
-
